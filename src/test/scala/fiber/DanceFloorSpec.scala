5141e6c84e60411a8850dd2b5cfb993fbbe861c4
Boris V.Kuznetsov <kb@infra.guru>
2019-10-29 17:05:32 +0300

    Upd dancers

--- src/test/scala/fiber/DanceFloorSpec.scala
+++ src/test/scala/fiber/DanceFloorSpec.scala
@@ -1,15 +1,17 @@
 package fiber
 
+import zio.duration._
 import zio.test.{ assert, suite, testM, DefaultRunnableSpec }
 import zio.test.Assertion.{ isTrue }
+import zio.test.TestAspect.{ timeout }
 
 object DanceFloorSpec
     extends DefaultRunnableSpec(
       suite("DanceFloorSpec")(
-        testM("sayHello correctly displays output") {
+        testM("Shows how to dance with Fibers") {
           for {
             out <- Party.party
           } yield assert(out, isTrue)
-        }
+        } @@ timeout(15.seconds)
       )
     )
