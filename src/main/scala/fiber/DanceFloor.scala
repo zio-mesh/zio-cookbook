246fcd54031f1438ef570ebd527f560378349dff
Boris V.Kuznetsov <kb@infra.guru>
2019-10-29 20:15:52 +0300

    Fixed console output with Live env

--- src/main/scala/fiber/DanceFloor.scala
+++ src/main/scala/fiber/DanceFloor.scala
@@ -1,7 +1,7 @@
 package fiber
 
 import zio.{ ZIO }
-import fiber.simplesemaphore.{ S }
+import fiber.simplesemaphore._
 
 import zio.duration.Duration
 import zio.clock._
@@ -9,13 +9,22 @@ import zio.console._
 import zio.random._
 
 object Party {
+  val fibers    = 4
+  val dancers   = 10L
+  val danceTime = 100000L
+
   val party = for {
-    dancefloor <- S(10)
-    dancers <- ZIO.foreachPar(1 to 100) { i =>
-                dancefloor.P *> nextDouble.map(d => Duration.fromNanos((d * 1000000).round)).flatMap { d =>
-                  putStrLn(s"${i} checking my boots") *> sleep(d) *> putStrLn(s"${i} dancing like it's 99")
-                } *> dancefloor.V
-              }
+    dancefloor <- S(dancers)
+    disco <- ZIO.foreachPar(1 to fibers) { i =>
+              dancefloor.P *> // decrease semaphore counter
+                // The block below represents a Monadic semantics and runs sequentially in a single fiber
+                nextDouble.map(d => Duration.fromNanos((d * 100000).round)).flatMap { d =>
+                  putStrLn(s"${i} checking my boots") *>   // effect 1
+                    sleep(d) *>                            // effect 2
+                    putStrLn(s"${i} dancing like it's 99") // effect 3
+                } *>
+                dancefloor.V // increase semaphore counter
+            }
   } yield true
 
 }
--- src/test/scala/fiber/DanceFloorSpec.scala
+++ src/test/scala/fiber/DanceFloorSpec.scala
@@ -4,13 +4,14 @@ import zio.duration._
 import zio.test.{ assert, suite, testM, DefaultRunnableSpec }
 import zio.test.Assertion.{ isTrue }
 import zio.test.TestAspect.{ timeout }
+import zio.test.environment.Live
 
 object DanceFloorSpec
     extends DefaultRunnableSpec(
       suite("DanceFloorSpec")(
         testM("Shows how to dance with Fibers") {
           for {
-            out <- Party.party
+            out <- Live.live(Party.party)
           } yield assert(out, isTrue)
         } @@ timeout(15.seconds)
       )
