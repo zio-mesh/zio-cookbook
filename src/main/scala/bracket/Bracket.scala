db3b041a3fcb4e85089e8e55495e9bcd8b87b2d7
Boris V.Kuznetsov <kb@infra.guru>
2019-10-31 17:58:05 +0300

    Added bracket example

---
+++ src/main/scala/bracket/Bracket.scala
@@ -0,0 +1,23 @@
+package bracket
+
+import zio.{ Task, UIO }
+import java.io.FileInputStream
+import java.nio.charset.StandardCharsets
+
+object BracketLib {
+
+  def closeStream(is: FileInputStream) =
+    UIO(is.close())
+
+  // helper method to work around in Java 8
+  def readAll(fis: FileInputStream, len: Long): Array[Byte] = {
+    val content: Array[Byte] = Array.ofDim(len.toInt)
+    fis.read(content)
+    content
+  }
+
+  def convertBytes(is: FileInputStream, len: Long) =
+    Task.effect(println(new String(readAll(is, len), StandardCharsets.UTF_8))) // Java 8
+  //Task.effect(println(new String(is.readAllBytes(), StandardCharsets.UTF_8))) // Java 11+
+
+}
---
+++ src/test/scala/bracket/BracketSpec.scala
@@ -0,0 +1,30 @@
+package bracket
+
+import java.io.{ File, FileInputStream }
+
+import zio.{ IO, Task }
+import zio.test.{ assert, assertM, suite, testM, DefaultRunnableSpec }
+import zio.test.Assertion.{ equalTo, succeeds }
+
+object BrackSpec
+    extends DefaultRunnableSpec(
+      suite("Bracket Spec")(
+        testM("String IO bracket") {
+          for {
+            str <- IO.succeed("Hello").bracket(_ => IO.succeed("Fail"))(_ => IO.succeed("Pass")).run
+            _   = println(str)
+          } yield assert(str, succeeds(equalTo("Pass")))
+
+        },
+        testM("File access bracket") {
+          val mybracket = for {
+            file   <- Task(new File("/tmp/hello"))
+            len    = file.length
+            string <- Task(new FileInputStream(file)).bracket(BracketLib.closeStream)(BracketLib.convertBytes(_, len))
+          } yield string
+
+          assertM(mybracket.run, equalTo(List("one", "two")))
+
+        }
+      )
+    )
