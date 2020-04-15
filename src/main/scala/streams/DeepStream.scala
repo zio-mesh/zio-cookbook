68fab47e3f6404b4314490adb54d85a37ee44d56
Boris V.Kuznetsov <kb@infra.guru>
2020-04-15 21:08:40 +0300

    Finished deep stream

--- src/main/scala/streams/DeepStream.scala
+++ src/main/scala/streams/DeepStream.scala
@@ -1,65 +1,89 @@
-package streams
-
-import java.util.concurrent.TimeUnit
+package streamsDemo
 
 import scala.{ Stream => _ }
 
-import zio.clock.Clock
+import Helper._
+
+import zio.clock.{ sleep }
+import zio.console.Console
 import zio.console.putStrLn
-import zio.duration.Duration
-import zio.stream._
-import zio.{ Runtime, Task, URIO, ZIO }
+import zio.duration._
+import zio.stream.{ Stream, ZStream }
+import zio.{ IO, Promise, Runtime, UIO, ZIO }
 
-trait Common {
-  val e = new RuntimeException("boom")
-
-  val compute: URIO[Clock, Unit] = ZIO.sleep(Duration(5, TimeUnit.SECONDS))
-
-  val failM: ZIO[Any, String, Nothing]               = ZIO.fail("Failed")
-  val failS: ZStream[Any, RuntimeException, Nothing] = ZStream.fail(e)
+final case class Channel(id: Int) {
+  def block(): Boolean = id % 2 == 0
 }
 
-case class Channel(id: Int)   extends Common
-case class Followers(id: Int) extends Common
+final case class Follower(id: Int) {
+  def block(): Boolean = id % 3 == 0
+}
 
-case class User(ch: Seq[Channel], flw: Seq[Followers])
+// Define Stream Workers TypeClass
+trait Worker[A] {
+  def procEntity(list: List[A]): ZIO[Console, Nothing, List[A]]
+}
+object Worker {
+  implicit val chWork = new Worker[Channel] {
+    def procEntity(list: List[Channel]): ZIO[zio.console.Console, Nothing, List[Channel]] =
+      for {
+        channelOut <- Stream
+                       .fromIterable(list)
+                       .mapM(item => if (item.block) IO.fail(s"Failed for $item.id") else UIO.succeed(item))
+                       .buffer(bufferDepth)
+                       .process
+                       .use(nPulls(_, procDepth))
+        filtered = channelOut.collect { case Right(value) => value }
+      } yield filtered
+  }
 
-class Manager() {
-  def blockUser(user: User): Unit = ???
-
-  def readPass[A](users: Stream[Throwable, A])  = ???
-  def blockPass[A](users: Stream[Throwable, A]) = ???
+  implicit val flwWork = new Worker[Follower] {
+    def procEntity(list: List[Follower]): ZIO[zio.console.Console, Nothing, List[Follower]] =
+      for {
+        channelOut <- Stream
+                       .fromIterable(list)
+                       .mapM(item => if (item.block) IO.fail(s"Failed for $item.id") else UIO.succeed(item))
+                       .buffer(bufferDepth)
+                       .process
+                       .use(nPulls(_, procDepth))
+        filtered = channelOut.collect { case Right(value) => value }
+      } yield filtered
+  }
 }
 
 object App1 extends App {
+
   val rt = Runtime.default
-  val e  = new RuntimeException("boom")
 
-  val N = 1
-  val M = 1
-  val P = 2
+  val channels  = (1 to N).map(Channel).toList
+  val followers = (1 to M).map(Follower).toList
 
-  val channels  = (1 to N).map(Channel)
-  val followers = (1 to M).map(Followers)
-  val users     = List.fill(P)(User(channels, followers))
-
-  val rand = scala.util.Random
-
-  // FIXME Make failing
-  def blockById(id: Int): Task[Int] = {
-    rand.nextInt(10)
-    Task.succeed(id)
-  }
-
-  def procUser(user: User) /* : Task[Unit] */ =
-    for {
-      chStream <- Stream.fromIterable(channels).runCollect.fork
-      // folStream <- Stream.fromIterable(followers).ru
-    } yield ()
+  // Abstract Processor
+  def doWork[A: Worker](entries: List[A]) = implicitly[Worker[A]].procEntity(entries)
 
   val app = for {
-    userStream <- Stream.fromIterable(users).foreach(procUser)
+    latch <- Promise.make[Nothing, Unit]
+
+    // Process Channels in a Separate fiber, sleep 1 second and set the latch
+    _ <- (doWork(channels).fork <* sleep(1.second)).flatMap(_ => latch.succeed(()))
+
+    // Followers are processed in own fiber, which starts only after channels finish
+    followersOut <- doWork(followers).fork.ensuring(latch.succeed(()))
+
+    // Get followers result from its fiber
+    followers <- followersOut.join
+    _         <- putStrLn(followers.toString)
   } yield ()
 
   rt.unsafeRun(app)
+}
+
+object Helper {
+  val N           = 10
+  val M           = 16
+  val bufferDepth = 2
+  val procDepth   = 8
+
+  def nPulls[R, E, A](pull: ZStream.Pull[R, E, A], n: Int): ZIO[R, Nothing, List[Either[Option[E], A]]] =
+    ZIO.foreach(1 to n)(_ => pull.either)
 }
