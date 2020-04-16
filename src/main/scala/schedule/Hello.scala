6e6d52a1edd22ddde163d7b58003b33e210ed42f
Boris V.Kuznetsov <kb@infra.guru>
2020-04-16 23:36:57 +0300

    Add retry example

--- src/main/scala/schedule/Hello.scala
+++ src/main/scala/schedule/Hello.scala
@@ -1,27 +1,39 @@
 package schedule
 
-import org.joda.time.DateTime
-
 import zio.console.putStrLn
-import zio.duration._
-import zio.{ App, Schedule }
+import zio.{ App, ZIO }
+import zio.blocking.blocking
+import Common._
 
 object App0 extends App {
   def run(args: List[String]) =
-    app.as(0)
-
-  def getSecondsTimestamp() = new DateTime().getSecondOfDay()
-
-  val eff0 = putStrLn(s"Simple Tick: $getSecondsTimestamp")
-  val eff1 = putStrLn(s"Timed  Tick: $getSecondsTimestamp")
-
-  val simpleSched = Schedule.recurs(3)
-  val timedSched  = Schedule.exponential(1.second) <* Schedule.recurs(3)
+    app.fold(_ => 1, _ => 0)
 
   val app = for {
-    eff0 <- eff0.repeat(simpleSched)
-    eff1 <- eff1.repeat(timedSched)
-    _    <- putStrLn(eff0.toString)
-    _    <- putStrLn(eff1.toString)
+    res0 <- eff0.repeat(simpleSched)
+    res1 <- eff1.repeat(timedSched)
+    _    <- putStrLn(res0.toString)
+    _    <- putStrLn(res1.toString)
   } yield ()
+}
+
+object App1 extends App {
+  def run(args: List[String]) =
+    app3.fold(_ => 1, _ => 0)
+
+  val app0 = blocking(eff0.retry(simpleSched).tap(msg => putStrLn(msg.toString)))
+  val app1 = blocking(eff1.retry(timedSched).tap(msg => putStrLn(msg.toString)))
+  val app2 = blocking(eff2.retry(simpleSched).tap(msg => putStrLn(msg.toString)))
+  val app3 = blocking(eff3.retry(timedSched).tap(msg => putStrLn(msg.toString)))
+  // val app0 = for {
+  //   fiber0 <- blocking(eff0.retry(simpleSched).tap(msg => putStrLn(msg.toString))).fork
+  //   fiber1 <- blocking(eff1.retry(timedSched).tap(msg => putStrLn(msg.toString))).fork
+  //   fiber2 <- blocking(eff2.retry(simpleSched).tap(msg => putStrLn(msg.toString))).fork
+  //   fiber3 <- blocking(eff3.retry(timedSched).tap(msg => putStrLn(msg.toString))).fork
+  //   _      <- fiber0.join
+  //   _      <- fiber1.join
+  //   _      <- fiber2.join
+  //   _      <- fiber3.join
+  // } yield ()
+
 }
